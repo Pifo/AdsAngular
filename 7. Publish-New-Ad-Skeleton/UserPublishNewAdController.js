93c1fd5433fdebc954ad9c3c2b324031350342d7
Pifo <alabalaivo@gmail.com>
2015-01-10 06:59:15 +0200

    Controller

---
+++ 7. Publish-New-Ad-Skeleton/UserPublishNewAdController.js
@@ -0,0 +1,23 @@
+'use strict';
+
+app.controller('UserPublishNewAdController',
+  function ($scope, $location, townsService, categoriesService, userService, notifyService) {
+      $scope.adData = {townId: null, categoryId: null};
+      $scope.categories = categoriesService.getCategories();
+      $scope.towns = townsService.getTowns();
+
+      $scope.publishAd = function(adData) {
+          userService.createNewAd(adData,
+              function success() {
+                  // TODO: show “success” info message
+				  notifyService.showInfo("Ad published");
+                  $location.path("/user/ads");	
+              },
+              function error(err) {
+                  // TODO: show error message
+					notifyService.showError("Ad failed",err);
+              }
+          );
+      };
+  }
+);
